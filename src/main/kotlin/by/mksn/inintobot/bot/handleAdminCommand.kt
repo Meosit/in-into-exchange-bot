419101ebdbe23e30c2e018b993e1ca0006b7f022
Mikhail Snitavets <mksn13@gmail.com>
2020-06-28 22:14:24 +0300

    Refactor admin commands

--- src/main/kotlin/by/mksn/inintobot/bot/handleAdminCommand.kt
+++ src/main/kotlin/by/mksn/inintobot/bot/handleAdminCommand.kt
@@ -24,30 +24,17 @@ private fun BotUser.toChatString() = """
     .toSimpleString()}`
     Query: `${lastQuery.trimToLength(25, "…")}`
     Requests: `${numRequests}` (chat: `${numRequests - inlineRequests}`; inline: `${inlineRequests}`)
-    Settings: `${settings?.let { AppContext.json.stringify(UserSettings.serializer(), it).escapeMarkdown() }}`
+    Settings: `${settings?.let {
+    if (settings != UserSettings(language = settings.language))
+        AppContext.json.stringify(UserSettings.serializer(), it).escapeMarkdown()
+    else "<same as default (${settings.language})>"
+}}`
 """.trimIndent()
 
 suspend fun Message.handleAdminCommand(sender: BotOutputSender): Boolean = when (text) {
-    "/reload" -> {
-        AppContext.exchangeRates.reloadAll(AppContext.httpClient, AppContext.json)
-        val markdown = AppContext.exchangeRates.ratesStatus.asSequence()
-            .map { (api, updated) ->
-                "${api.name}: ${updated.lastChecked.withZoneSameInstant(ZoneId.of("UTC+3")).toSimpleString()}"
-            }
-            .joinToString(separator = "\n", prefix = "Last updated:\n```\n", postfix = "\n```")
-        sender.sendChatMessage(AppContext.creatorId, BotTextOutput(markdown))
-        true
-    }
     "/me" -> {
         val user = UserStore.userById(AppContext.creatorId.toLong())
         val markdown = user?.toChatString() ?: "Not found"
-        sender.sendChatMessage(AppContext.creatorId, BotTextOutput(markdown))
-        true
-    }
-    "/last5" -> {
-        val users = UserStore.lastUsed(6).drop(1)
-        val markdown = users
-            .joinToString(separator = "\n---\n", prefix = "Last 5 users except admin:\n") { it.toChatString() }
         sender.sendChatMessage(AppContext.creatorId, BotTextOutput(markdown))
         true
     }
@@ -59,46 +46,46 @@ suspend fun Message.handleAdminCommand(sender: BotOutputSender): Boolean = when 
         sender.sendChatMessage(AppContext.creatorId, BotTextOutput(markdown))
         true
     }
-    "/lastday" -> {
-        val users = UserStore.lastUsed(100, 24).drop(1)
-        val markdown = if (users.isNotEmpty()) users
-            .joinToString(separator = "\n---\n", prefix = "Users for the last 24 hours:\n") { it.toChatString() }
-        else "No users for the last 24 hours"
-        sender.sendChatMessage(AppContext.creatorId, BotTextOutput(markdown))
-        true
-    }
     else -> when {
-        text != null && text.startsWith("/reload ") -> {
-            val apiAlias = text.removePrefix("/reload ")
-            val rateApi = AliasMatcher(AppContext.supportedApis).matchOrNull(apiAlias)
-            val markdown = if (rateApi != null) {
-                AppContext.exchangeRates.reloadOne(rateApi, AppContext.httpClient, AppContext.json)
-                AppContext.exchangeRates.ratesStatus.asSequence()
-                    .filter { it.key == rateApi }
+        text != null && text matches "/last\\d+".toRegex() -> {
+            val users = UserStore.lastUsed(text.removePrefix("/last").toInt()).drop(1)
+            val markdown = if (users.isNotEmpty())
+                users.joinToString(separator = "\n---\n", prefix = "Last 5 users except admin:\n") { it.toChatString() }
+            else "No users"
+            sender.sendChatMessage(AppContext.creatorId, BotTextOutput(markdown))
+            true
+        }
+        text != null && text.startsWith("/reload") -> {
+            val apiAlias = text.removePrefix("/reload")
+            if (apiAlias.isEmpty()) {
+                AppContext.exchangeRates.reloadAll(AppContext.httpClient, AppContext.json)
+                val markdown = AppContext.exchangeRates.ratesStatus.asSequence()
                     .map { (api, updated) ->
                         "${api.name}: ${updated.lastChecked.withZoneSameInstant(ZoneId.of("UTC+3")).toSimpleString()}"
                     }
-                    .ifEmpty { sequenceOf("API was never updated") }
                     .joinToString(separator = "\n", prefix = "Last updated:\n```\n", postfix = "\n```")
+                sender.sendChatMessage(AppContext.creatorId, BotTextOutput(markdown))
             } else {
-                "Invalid API alias provided"
+                val rateApi = AliasMatcher(AppContext.supportedApis).matchOrNull(apiAlias)
+                val markdown = if (rateApi != null) {
+                    AppContext.exchangeRates.reloadOne(rateApi, AppContext.httpClient, AppContext.json)
+                    AppContext.exchangeRates.ratesStatus.asSequence()
+                        .filter { it.key == rateApi }
+                        .map { (api, updated) ->
+                            "${api.name}: ${updated.lastChecked.withZoneSameInstant(ZoneId.of("UTC+3"))
+                                .toSimpleString()}"
+                        }
+                        .ifEmpty { sequenceOf("API was never updated") }
+                        .joinToString(separator = "\n", prefix = "Last updated:\n```\n", postfix = "\n```")
+                } else {
+                    "Invalid API alias provided"
+                }
+                sender.sendChatMessage(AppContext.creatorId, BotTextOutput(markdown))
             }
-            sender.sendChatMessage(AppContext.creatorId, BotTextOutput(markdown))
             true
         }
-        text != null && text.startsWith("/user ") -> {
-            val id = text.removePrefix("/user ")
-            val markdown = try {
-                val user = UserStore.userById(id.toLong())
-                user?.toChatString() ?: "Not found"
-            } catch (e: Exception) {
-                "Unable to select: ${e::class.simpleName} ${e.message ?: e.cause?.message}"
-            }
-            sender.sendChatMessage(AppContext.creatorId, BotTextOutput(markdown))
-            true
-        }
-        text != null && text.startsWith("/select ") -> {
-            val whereClause = text.removePrefix("/select ")
+        text != null && text.startsWith("/select") -> {
+            val whereClause = text.removePrefix("/select")
             val markdown = try {
                 val users = UserStore.usersGenericSelect(whereClause)
                 users.joinToString(separator = "\n---\n", prefix = "Selected:\n") { it.toChatString() }
@@ -108,8 +95,8 @@ suspend fun Message.handleAdminCommand(sender: BotOutputSender): Boolean = when 
             sender.sendChatMessage(AppContext.creatorId, BotTextOutput(markdown))
             true
         }
-        text != null && text.startsWith("/count ") -> {
-            val whereClause = text.removePrefix("/count ")
+        text != null && text.startsWith("/count") -> {
+            val whereClause = text.removePrefix("/count")
             val markdown = try {
                 val count = UserStore.usersCount(whereClause)
                 "Count is $count"
